d0138e46c33454e6fe7616919dd1fb3cf54fbe87
Taylor Calderone <spacecowgoesmoo@gmail.com>
2020-03-29 15:27:40 -0700

    NEW SHADER IS DONE WOOOOOOOOOOO

--- shaders.js
+++ shaders.js
@@ -1,116 +1,49 @@
-// created by weepy from the PIXI.js github
-// https://github.com/pixijs/pixi.js/issues/2216
-
-// var src =
-//     'precision mediump float;' +
-//     'varying vec2 vTextureCoord;' +
-//     'varying vec2 vFilterCoord;' +
-//     'uniform mat3 mappedMatrix;' +
-//     'uniform sampler2D uSampler;' +
-
-//     // start and end colors
-//     'uniform vec3 tint1;' +
-//     'uniform vec3 tint2;' +
-
-//     'void main(){' +
-
-// 	// vec2 uvs = vTextureCoord.xy;
-//     'vec3 mapCoord = vec3(vTextureCoord, 1.0) * mappedMatrix;' +
-//     'vec3 mixCol = mix(tint1, tint2, mapCoord.y);' +
-
-//     'vec4 fg = texture2D(uSampler, vTextureCoord);' +
-
-//     'gl_FragColor = fg*vec4(mixCol, 1.0);' +
-//     '}';
-
-
-// New recommendation
-// ((vTextureCoord * inputSize.xy) + outputFrame.xy) / outputFrame.zw
-// Dumb rewrite for IE compatibility. ` is unsupported
-var src =
+// Annoyingly reformatted for IE compatibility. ` is unsupported
+const shaderSourceCode =
 	// Included data sources
-    'precision mediump float;' +
-    'varying vec2 vTextureCoord;' +	// The coordinates of the current pixel
-    'varying vec2 vFilterCoord;' +
-    'uniform mat3 mappedMatrix;' +
-    'uniform sampler2D uSampler;' +	// The image data
-    'uniform vec4 inputSize;' +
-    'uniform vec4 outputFrame;' +
-    // Start and end colors
-    'uniform vec3 tint1;' +
-    'uniform vec3 tint2;' +
+	'precision mediump float;' +
+	'varying vec2 vTextureCoord;' +		// The coordinates of the current pixel
+	'uniform sampler2D uSampler;' +		// The image data
+	'float screenHeight = 600.0;' +		// Pixels
+
+	// Start and end colors
+	'uniform vec3 tint1;' +				// Vector3 containg RGB values
+	'uniform vec3 tint2;' +
 
     // Actual graphics code
-    'void main(){' +
-    	// The new patch
-   		'vec2 cow = (((vTextureCoord * inputSize.xy) + outputFrame.xy) / outputFrame.zw);' +
+	'void main(void) {' +
+		'float yPercent = vTextureCoord.y / screenHeight;' +				// Get the height of the next pixel to draw, scaled 0 to 1
+		'vec3 newPixelByColumn = mix(tint1, tint2, yPercent);' +			// Prepare the next pixel in the gradient column
+		'vec4 nextBlankPixel = texture2D(uSampler, vTextureCoord);' +		// Grab the next pixel on the canvas
+    	'gl_FragColor = nextBlankPixel * vec4(newPixelByColumn, 1.0);' +	// Multiply things together to paint it
+	'}';
 
-		// vec2 uvs = vTextureCoord.xy;
-    	'vec3 mapCoord = vec3(vTextureCoord, 1.0) * mappedMatrix;' +
-    	'vec3 mixCol = mix(tint1, tint2, mapCoord.y);' +
 
- 		'vec4 fg = texture2D(uSampler, vTextureCoord);' +
-
-    	'gl_FragColor = fg*vec4(mixCol, 1.0);' +
-    '}';
 
 
 const plainGradient = function (start, end) {
-    // Get shader code as a string, and create our Pixi filter
-    var shader = new PIXI.Filter(null, src, {
-        // These are the shader 'uniforms'
-        tint1 : { type: 'v3', value: new Float32Array([1.0, 1.0, 1.0]) },
-        tint2 : { type: 'v3', value: new Float32Array([0.0, 0.0, 0.0]) },
-        mappedMatrix: {
-        	type: 'mat3',
-			value: new PIXI.Matrix()
-        }
-    });
+	// Get shader code as a string, and create our Pixi filter
+	var cowShader = new PIXI.Filter(null, shaderSourceCode, {
+		// These are the shader 'uniforms'
+		tint1 : { type: 'v3', value: new Float32Array([1.0, 1.0, 1.0]) },
+		tint2 : { type: 'v3', value: new Float32Array([0.0, 0.0, 0.0]) },
+	});
 
+	function formatRGBColor(x) {
+		// Input: 0xFF9933
+		// Output: [0.123, 0.456, 0.789]
+		const r = (x&0xff0000|0)/0xff0000
+		const g = (x&0xff00|0)/0xff00
+		const b = (x&0xff|0)/0xff
 
+		return new Float32Array([r,g,b])
+	}
 
+	if (arguments.length > 0) {				// if statement is just a safety measure
+		cowShader.uniforms.tint1 = formatRGBColor(start)
+		cowShader.uniforms.tint2 = formatRGBColor(end)
+	}
 
-    function rgb(x) {
-        const r = (x&0xff0000|0)/0xff0000
-        const g = (x&0xff00|0)/0xff00
-        const b = (x&0xff|0)/0xff
-
-        return new Float32Array([r,g,b])
-    }
-
-    shader.apply = function(filterManager, input, output, currentState) {
-    	// Deprecated
-		// This does: "Multiply vTextureCoord to this matrix to achieve (0,0,1,1) for filterArea"
-		// filterManager.calculateNormalizedScreenSpaceMatrix(this.uniforms.mappedMatrix);
-
-		// New recommendation
-		// ((vTextureCoord * inputSize.xy) + outputFrame.xy) / outputFrame.zw
-
-		// I think this works, except currentState (below) isn't being fetched properly
-		// It's a copypaste of the deprecated function
-		// function patch2020(inputMatrix, filterArea, textureSize) {
-  //   		const mappedMatrix = inputMatrix.value.identity();
-		//     mappedMatrix.translate(filterArea.x / textureSize.width, filterArea.y / textureSize.height);
-		//     const translateScaleX = (textureSize.width / filterArea.width);
-  //   		const translateScaleY = (textureSize.height / filterArea.height);
-		//     mappedMatrix.scale(translateScaleX, translateScaleY);
-		//     return mappedMatrix;
-		// }
-
-  //   	console.log(currentState)
-		// const filterArea = currentState.sourceFrame;
-  //   	const textureSize = currentState.destinationFrame;
-		// this.uniforms.mappedMatrix = patch2020(this.uniforms.mappedMatrix, filterArea, textureSize);
-		filterManager.applyFilter(this, input, output);
-    };
-
-    shader.setColors = function(start, end) {
-        shader.uniforms.tint1 = rgb(start)
-        shader.uniforms.tint2 = rgb(end)
-    }    
-
-    if (arguments.length > 0) {
-        shader.setColors(start, end)
-    }
-    return shader
+	console.log('Generating new background - (' + start + ', ' + end + ')')
+	return cowShader
 }
